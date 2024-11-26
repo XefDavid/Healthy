dda97c242be6ec598177ec25950964a62b87046b
David <156122937+XefDavid@users.noreply.github.com>
2024-11-26 08:52:59 +0100

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import "primeicons/primeicons.css";
+</script>
+
+<template>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
+</template>
+<style>
+@import "~/assets/css/main.css";
+</style>
